d16bb8ec7e61707b2f99b659481f61558d95e714
mik14a <mik14a@gmail.com>
2018-12-15 18:25:44 +0900

    Read frame rotation and translation

--- include/node/transform.h
+++ include/node/transform.h
@@ -11,13 +11,23 @@ namespace vox {
  * Transform Node Chunk
  */
 struct transform : node {
+  struct frame {
+    struct {
+      int8_t m[3][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    } rotation;
+    struct {
+      int32_t x = 0, y = 0, z = 0;
+    } translation;
+  };
+
   static constexpr uint32_t tag = generate_id('n', 'T', 'R', 'N');
+  using frame_t = std::vector<transform::frame>;
 
   std::string name;
   bool hidden;
   int32_t child;
   int32_t layer;
-  std::vector<std::unordered_map<std::string, std::string>> frame;
+  frame_t frame;
 
   transform() { node::tag = tag; }
   static transform* read(const void*& data, size_t& size);
--- source/node/transform.cpp
+++ source/node/transform.cpp
@@ -1,5 +1,7 @@
 #include "node/transform.h"
 
+#include <sstream>
+
 namespace vox {
 
 transform* transform::read(const void*& data, size_t& size) {
@@ -17,8 +19,35 @@ transform* transform::read(const void*& data, size_t& size) {
   read_t<int32_t>(data, size);  // reserved
   ntrn->layer = read_t<int32_t>(data, size);
   auto frames = read_t<int32_t>(data, size);
-  for (auto i = 0; i < frames; ++i) {
-    auto frame = read_dictionary(data, size);
+  for (auto f = 0; f < frames; ++f) {
+    auto dictionary = read_dictionary(data, size);
+    auto frame = frame_t::value_type();
+    auto r = dictionary.find("_r");
+    if (r != dictionary.end()) {
+      const auto m = std::stoi(r->second.c_str());
+      const auto first = static_cast<int>((m >> 0) & 0x03);
+      const auto second = static_cast<int>((m >> 2) & 0x03);
+      const auto third = 0x03 ^ (first | second);
+      const int sign[] = {
+          static_cast<int>((m >> 4) & 0x01),
+          static_cast<int>((m >> 5) & 0x01),
+          static_cast<int>((m >> 6) & 0x01),
+      };
+      for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+          frame.rotation.m[i][j] = 0;
+        }
+      }
+      frame.rotation.m[0][first] = sign[0] ? -1 : 1;
+      frame.rotation.m[1][second] = sign[1] ? -1 : 1;
+      frame.rotation.m[2][third] = sign[2] ? -1 : 1;
+    }
+    auto t = dictionary.find("_t");
+    if (t != dictionary.end()) {
+      auto stream = std::stringstream(t->second);
+      stream >> frame.translation.x >> frame.translation.y >>
+          frame.translation.z;
+    }
     ntrn->frame.push_back(std::move(frame));
   }
   return ntrn;
